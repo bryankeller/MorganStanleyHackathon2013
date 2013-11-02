cc0290c801a995499c271b1539b565b26a5ce384
Evan Hopkins <Hopkins@The-Soviet-Union.local>
2013-11-02 13:07:53 -0400

    asdf

--- hermes.py
+++ hermes.py
@@ -31,31 +31,64 @@ def processTurn(num):
 	posts = []
 
 	while True:
+		lastReply = play()
+		replies.append(lastReply)
 		emptyPost = JSONPostData()
 		post = algorithm(emptyPost, replies) #returns the dict of json file
 		change(post)
-		lastReply = play()
-
-		replies.append(lastReply)
 		posts.append(post)
-		printStats(lastReply)
+		#printStats(lastReply)
 		#if((int(lastReply['ServerState']['TurnNo'])%num)==0):
 			#quickInfo(lastReply)
 	
 def algorithm(post, replies):
-	if len(replies):
-		lastReply = replies[-1]
+	newPost = JSONPostData()
+	lastReply = replies[len(replies)-1]
 
-		# --- Algorithm goes here ----
+	#generates a summary report every 15 turns
+	turnNo = int(lastReply['ServerState']['TurnNo'])
+	#print "Replies sizeee: "+str(len(replies))
+	#print "Turn Nono: "+ str(turnNo)
+	if((turnNo%15)==0 and turnNo!=0):
+		summary(replies);
 
-		if profitEarned(lastReply) > 100:
-			post.setWebNodeCounts(eu=10)
+	reply = newPost.getGeneratedJSONPostData()
+	return reply
 
+def summary(replies):
+	#print "Replies size: "+str(len(replies))
+	length = len(replies)
+	lastReply = replies[length-1]
+	naExecuted = euExecuted = apExecuted = naNodes = euNodes = apNodes = profit= 0
+	#replies = reversed(replies)
+	for i in range(length-15,length):
+		naExecuted = naExecuted + replies[i]["ServerState"]["ServerTiers"]["WEB"]["ServerRegions"]["NA"]["NoOfTransactionsInput"]
+		euExecuted = euExecuted + replies[i]["ServerState"]["ServerTiers"]["WEB"]["ServerRegions"]["EU"]["NoOfTransactionsInput"]
+		apExecuted = apExecuted + replies[i]["ServerState"]["ServerTiers"]["WEB"]["ServerRegions"]["AP"]["NoOfTransactionsInput"]
+		naSucceeded = naExecuted + replies[i]["ServerState"]["ServerTiers"]["WEB"]["ServerRegions"]["NA"]["NoOfTransactionsInput"]
+		euSucceeded = euExecuted + replies[i]["ServerState"]["ServerTiers"]["WEB"]["ServerRegions"]["EU"]["NoOfTransactionsInput"]
+		ap = apExecuted + replies[i]["ServerState"]["ServerTiers"]["WEB"]["ServerRegions"]["AP"]["NoOfTransactionsInput"]
+		naNodes = lastReply["ServerState"]["ServerTiers"]["WEB"]["ServerRegions"]["NA"]["NodeCount"]
+		euNodes = lastReply["ServerState"]["ServerTiers"]["WEB"]["ServerRegions"]["EU"]["NodeCount"]
+		apNodes = lastReply["ServerState"]["ServerTiers"]["WEB"]["ServerRegions"]["AP"]["NodeCount"]
+		profit = profit + int(replies[i]["ServerState"]["ProfitEarned"])
+		#print "Turn " + str(i) + "    " + str(replies[i]["ServerState"]["ProfitEarned"]) + "   " + str(profit)
 
+	lastReply = replies[length-1]
+	naExecuted = naExecuted/15  
+	euExecuted = euExecuted/15 
+	apExecuted = apExecuted/15 
+	profit = profit/15
+	bank = lastReply["ServerState"]["ProfitAccumulated"]
+	turnNo = int(lastReply['ServerState']['TurnNo'])
 
-		# ----------------------------
+	print "Turn: " + str(turnNo)
+	print "[NA]  Nodes:" + str(naNodes) + ", Executed: " + str(naExecuted)
+	print "[EU]  Nodes:" + str(euNodes) + ", Executed: " + str(euExecuted)
+	print "[AP]  Nodes:" + str(apNodes) + ", Executed: " + str(apExecuted)
+	print "Total profit $"+str(bank) + "     Average turn profit $" + str(profit)
+	print " "
 
-	return post.getGeneratedJSONPostData()
 
 
 
