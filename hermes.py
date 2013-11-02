23101b5d5755b981f10d9ef1a9ce89a1c8e19e75
Evan Hopkins <Hopkins@148-100-129-116.foxnet.marist.edu>
2013-11-01 20:13:03 -0400

    used methods

--- hermes.py
+++ hermes.py
@@ -1,11 +1,18 @@
 #pipe program through 'python -mjson.tool'
 import requests
-import json
 
-payload = {
-	'Command' : 'INIT',
-	'Token' : '63b92b83-041d-4fac-a4db-07be7219f604',
-	'ChangeRequest' : 'null'
-}
-reply = requests.post(r'http://hermes.wha.la/api/hermes', data=payload)
-print reply.text
+def main():
+	initReply = initializeGame()
+	print initReply.text
+
+def initializeGame():
+	payload = {
+		'Command' : 'INIT',
+		'Token' : '63b92b83-041d-4fac-a4db-07be7219f604',
+		'ChangeRequest' : 'null'
+	}
+	reply = requests.post(r'http://hermes.wha.la/api/hermes', data=payload)
+	return reply
+
+if __name__ == '__main__':
+	main()
