57a66a629a310e290b84174e51e20abde534896b
blkhp19 <kellerbryan19@gmail.com>
2013-11-02 16:33:15 -0400

    better

--- bryanAlg.py
+++ bryanAlg.py
@@ -3,16 +3,17 @@ def algorithm(jDict, replies):
 	euRateOfChange = 0
 	apRateOfChange = 0
 
-	# print replies[0].executedNums()
-	if(len(replies) % 15 == 0 and len(replies) > 0):
-		for num in range(len(replies) - 16, len(replies) - 1):
+	minuteInterval = 60;
+
+	if(len(replies) % minuteInterval == 0 and len(replies) > 0):
+		for num in range(len(replies) - (minuteInterval + 1), len(replies) - 1):
 			naRateOfChange += float(replies[num+1].executedNums()["na"]) - float(replies[num].executedNums()["na"])
 			euRateOfChange += float(replies[num+1].executedNums()["eu"]) - float(replies[num].executedNums()["eu"])
 			apRateOfChange += float(replies[num+1].executedNums()["ap"]) - float(replies[num].executedNums()["ap"])
 
-		naRateOfChange /= 14.00
-		euRateOfChange /= 14.00
-		apRateOfChange /= 14.00
+		naRateOfChange /= minuteInterval - 1
+		euRateOfChange /= minuteInterval - 1
+		apRateOfChange /= minuteInterval - 1
 
 		print "NA Executed Transactions Rate of Change: " + str(naRateOfChange)
 		print "EU Executed Transactions Rate of Change: " + str(euRateOfChange)
