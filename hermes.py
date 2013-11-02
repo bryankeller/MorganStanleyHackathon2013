778372e8f31ecf1fde3e3c21bcb9cfc8ffc88d04
Evan Hopkins <Hopkins@148-100-129-116.foxnet.marist.edu>
2013-11-01 23:15:46 -0400

    fully functioning dummy loop

--- hermes.py
+++ hermes.py
@@ -1,10 +1,12 @@
 #pipe program through 'python -mjson.tool'
 import requests
+import json
+from pprint import pprint
+import time
 
 def main():
-	initReply = initializeGame()
-	print initReply.text
-	#play()
+	initializeGame()
+	processTurn()
 
 def initializeGame():
 	payload = {
@@ -13,27 +15,55 @@ def initializeGame():
 		'ChangeRequest' : None
 	}
 	reply = requests.post(r'http://hermes.wha.la/api/hermes', data=payload)
+	replyDict = json.loads(reply.text)
+	print 'Game has started'
+	quickInfo(replyDict)
+	#wait()
 	return reply
 
-def processTurn(data):
-	json = x
+def processTurn():
+	while True:
+		change()
+		play()
+	
+	
 
+def change():
+	jsonDict = getDummyJson()
 	payload = {
 		'Command' : 'CHNG',
 		'Token' : '63b92b83-041d-4fac-a4db-07be7219f604',
-		'ChangeRequest' : None
+		'ChangeRequest' : jsonDict
 	}
 	reply = requests.post(r'http://hermes.wha.la/api/hermes', data=payload)
-	processTurn(reply)
 
 def play():
+	jsonDict = getDummyJson()
 	payload = {
 		'Command' : 'PLAY',
 		'Token' : '63b92b83-041d-4fac-a4db-07be7219f604',
 		'ChangeRequest' : None
 	}
-	requests.post(r'http://hermes.wha.la/api/hermes', data=payload)
-	play()
+	reply = requests.post(r'http://hermes.wha.la/api/hermes', data=payload)
+	replyDict = json.loads(reply.text)
+	if((int(replyDict['ServerState']['TurnNo'])%100)==0):
+		quickInfo(replyDict)
+
+def getDummyJson():
+	generatedJSON = json.load(open("standardJSON.json"))
+	return generatedJSON
+
+def wait():
+	idontgiveafuckinput = raw_input()
+
+def quickInfo(replyDict):
+	turn = replyDict['ServerState']['TurnNo']
+	bank = replyDict['ServerState']['ProfitAccumulated']
+	profit = replyDict['ServerState']['ProfitEarned']
+	print "Turn: "+str(turn)
+	print "Turn Profit: "+str(profit)
+	print "Bankroll Yo: "+str(bank)
+	print "-------------------"
 
 if __name__ == '__main__':
 	main()
