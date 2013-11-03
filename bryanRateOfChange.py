b1d6228d6c413536b42cbc36778e6da0f7f94734
blkhp19 <kellerbryan19@gmail.com>
2013-11-03 01:18:19 -0400

    such objective-c

--- bryanRateOfChange.py
+++ bryanRateOfChange.py
@@ -1,13 +1,13 @@
-def getJavaRateOfChange(replies):
-	naJavaRateOfChange = euJavaRateOfChange = apJavaRateOfChange = 0
+def getRateOfChangeForRepliesWithServerTypeSpecifiedAsString(replies, serverType):
+	naRateOfChange = euRateOfChange = apRateOfChange = 0
 
 	for num in range(0, len(replies) - 2):
-		naJavaRateOfChange += float(replies[num+1].executedNums("JAVA")["na"]) - float(replies[num].executedNums("JAVA")["na"])
-		euJavaRateOfChange += float(replies[num+1].executedNums("JAVA")["eu"]) - float(replies[num].executedNums("JAVA")["eu"])
-		apJavaRateOfChange += float(replies[num+1].executedNums("JAVA")["ap"]) - float(replies[num].executedNums("JAVA")["ap"])
+		naRateOfChange += float(replies[num+1].executedNums("serverType")["na"]) - float(replies[num].executedNums("serverType")["na"])
+		euRateOfChange += float(replies[num+1].executedNums("serverType")["eu"]) - float(replies[num].executedNums("serverType")["eu"])
+		apRateOfChange += float(replies[num+1].executedNums("serverType")["ap"]) - float(replies[num].executedNums("serverType")["ap"])
 
-	naJavaRateOfChange /= len(replies) - 1
-	euJavaRateOfChange /= len(replies) - 1
-	apJavaRateOfChange /= len(replies) - 1
+	naRateOfChange /= len(replies) - 1
+	euRateOfChange /= len(replies) - 1
+	apRateOfChange /= len(replies) - 1
 
-	return {'na' : naJavaRateOfChange, 'eu' : euJavaRateOfChange, 'ap' : apJavaRateOfChange}
+	return {'na' : naRateOfChange, 'eu' : euRateOfChange, 'ap' : apRateOfChange}
